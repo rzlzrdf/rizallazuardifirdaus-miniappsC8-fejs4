ed662e8ae5616ded60591794f2430b1a76eef610
rzlzrdf <rizallazu@student.ub.ac.id>
2022-06-09 13:15:12 +0700

    make a hero-section

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import Home from './components/pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-          <Route path='/' exact />
+          <Route path='/' exact element={<Home/>} />
       </Routes>
     </BrowserRouter>
   );
